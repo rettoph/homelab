ca47d3b1f437403abd8bb8f26a1aae181710bbb4
rettoph <anthony@turner.ec>
2025-03-16 22:00:11 -0600

    Add and configure nano

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # homelab
 Setup scripts to easily spin up new modules of my personal homelab
+
+---
+
+## Run via
+`wget -O install.sh https://raw.githubusercontent.com/rettoph/homelab/refs/heads/main/install.sh && chmod +x ./install.sh && ./install.sh`
--- install.sh
+++ install.sh
@@ -1,1 +1,15 @@
+local environmentFile="/etc/profile.d/custom.g.sh"
+
 beep
+
+# Remove custom profile config if it exists
+rm /etc/profile.d/environment.g.sh
+echo "# Generated via https://github.com/rettoph/homelab/blob/main/install.sh on $(date +%Y%m%d)" >> $environmentFile
+echo "# DO NOT EDIT DIRECTLY. This file will be overwritten next time the installer is ran." >> $environmentFile
+echo "" >> $environmentFile
+
+# Download nano & configure to be default editor
+echo "Installing nano..."
+apk add nano
+echo "# Set nano as default text editor" >> $environmentFile
+echo "export EDITOR=/usr/bin/nano" >> $environmentFile
